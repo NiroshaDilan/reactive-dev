e8240cde4902b9264fd7b9c6e05b461f626250cd
DESKTOP-IJ4FOE7\DILAN <dilan.vista@gmail.com>
2018-03-15 17:04:22 +0530

    Revert "Revert "changes""
    This reverts commit d6effb6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  myform: FormGroup;
+
+  langs: string[] = [
+    'English',
+    'French',
+    'German'
+  ];
+
+  ngOnInit(): void {
+    this.myform = new FormGroup({
+      name: new FormGroup({
+        firstName: new FormControl(),
+        lastName: new FormControl(),
+      }),
+      email: new FormControl(),
+      password: new FormControl(),
+      language: new FormControl()
+    });
+  }
 }
