24c2fa144d80f3d7b05a7756e5792187d2b65971
DESKTOP-IJ4FOE7\DILAN <dilan.vista@gmail.com>
2018-03-16 14:39:58 +0530

    Revert "Revert "joke list""
    This reverts commit 61ae99c

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { JokeFormComponent } from './component/joke/joke-form/joke-form.component';
+import { JokeListComponent } from './component/joke/joke-list/joke-list.component';
+import {JokeComponent} from './component/joke/joke/joke.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    JokeFormComponent,
+    JokeListComponent,
+    JokeComponent
   ],
   imports: [
     BrowserModule
